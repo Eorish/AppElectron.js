de1ab68b9db5c5a5430fc9b64ba89e2b20dc27e6
hasted.Py <85710610+hastedPy@users.noreply.github.com>
2021-06-13 22:29:08 -0400

    Add files via upload

---
+++ in.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+BrowserWindow.MaximizeBox = true;
+
+BrowserWindow.MinimizeBox = true;  
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        frame: true,
+        acceptFirstMouse: true,
+        modal: true,
+        title: "My new Application", icon:'icon.ico',
+    });
+
+    mainWindow.loadFile(__dirname + '/Example.html')
+    mainWindow.setMenu(null)
+    mainWindow.unmaximize(false);
+    mainWindow.isMaximized = false;
+    mainWindow.setResizable(false);
+    mainWindow.setMovable(false);
+    
+}
+app.on('ready', createWindow);
